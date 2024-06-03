6853c195189301b92c419c75a1ca203946a2f4b4
08leo08 <135195419+08leo08@users.noreply.github.com>
2024-06-03 18:45:59 -0400

    Add files via upload

---
+++ EXAMEN/SitioWeb/js/Formulario.js
@@ -0,0 +1,16 @@
+<form class="text-center">
+  <div class="form-group">
+    <label for="exampleInputEmail1" class="text-center">Correo Electronico</label>
+    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="">
+    <small id="emailHelp"</small>
+  </div>
+  <div class="form-group">
+    <label for="exampleInputPassword1" class="text-center">Password</label>
+    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="">
+  </div>
+  <div class="form-check d-flex justify-content-center align-items-center"> 
+    <input type="checkbox" class="form-check-input" id="exampleCheck1">
+    <label class="form-check-label" for="exampleCheck1">Recordarme</label>
+  </div>
+  <button type="submit" class="btn btn-primary">Enviar</button>
+</form>
